64ed7413b1d3959ef9deb700b6d197426de44b53
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 19:09:47 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan9-Teks/src/pboif2/pkg10119083/latihan9/teks/PBOIF210119083Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama      : Rafsan Zen Mustaofa
+ * Kelas     : IF2
+ * NIM       : 10119083
+ * Deskripsi : Menampilkan teks
+ */
+public class PBOIF210119083Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'S';
+        System.out.println("Hasil" + newline + "char :" + tab + "berisi  " + c);
+    }
+    
+}
